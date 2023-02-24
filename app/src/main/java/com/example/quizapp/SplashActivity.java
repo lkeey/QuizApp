c1fa29a299fbd7964aa350a0e793353d2b0d30d4
l_keey14 <Leon062007@mail.ru>
2023-02-24 13:20:46 +0300

    create animation; add bottom menu

---
+++ app/src/main/java/com/example/quizapp/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        appName = findViewById(R.id.appName);
+
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.blacklist);
+        appName.setTypeface(typeface);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.my_anim);
+        appName.setAnimation(animation);
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+
+            SplashActivity.this.finish();
+
+        }).start();
+    }
+}
